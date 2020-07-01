7ce9b7337b7c26b68956be13fbaa70563dad919e
Rian Perassoli <rian.perassoli@gmail.com>
2020-06-30 21:54:19 -0300

    Criado estilo global

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 
-const App: React.FC = () => <h1>APP</h1>
+import GlobalStyle from './styles/global'
+
+const App: React.FC = () => (
+  <>
+    <h1>APP</h1>
+    <GlobalStyle />
+  </>
+)
 
 export default App
