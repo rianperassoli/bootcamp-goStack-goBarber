1d129e82b540647aef9defd801f8205e0229179c
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-01 18:15:43 -0300

    Criada a tela de signup

---
+++ web/src/pages/SignUp/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { FiMail, FiLock, FiUser, FiArrowLeft } from 'react-icons/fi'
+import { Form } from '@unform/web'
+
+import logoImg from '../../assets/logo.svg'
+
+import { Container, Content, Background } from './styles'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+const SignUp: React.FC = () => {
+  const handleSubmit = (data: object): void => {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <Background />
+
+      <Content>
+        <img src={logoImg} alt="GoBarber" />
+
+        <Form onSubmit={handleSubmit}>
+          <h1>Faça seu cadastro</h1>
+
+          <Input name="name" icon={FiUser} placeholder="Nome" />
+
+          <Input name="email" icon={FiMail} placeholder="E-mail" />
+
+          <Input
+            name="password"
+            icon={FiLock}
+            type="password"
+            placeholder="Senha"
+          />
+
+          <Button type="submit">Cadastrar</Button>
+        </Form>
+
+        <a href="login">
+          <FiArrowLeft />
+          Voltar para logon
+        </a>
+      </Content>
+    </Container>
+  )
+}
+
+export default SignUp
