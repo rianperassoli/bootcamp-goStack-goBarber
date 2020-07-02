2b9242b504947c3fb62bd9f6b081c6cf5522753a
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-01 22:34:58 -0300

    Passa a autenticar na api e logar dados de autenticação

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -10,13 +10,18 @@ import Button from '../../components/Button'
 import getValidationErrors from '../../utils/getValidationErrors'
 import { AuthContext } from '../../context/AuthContext'
 
+interface SignInFormData {
+  email: string
+  password: string
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null)
 
   const { signIn } = useContext(AuthContext)
 
   const handleSubmit = useCallback(
-    async (data: object) => {
+    async (data: SignInFormData) => {
       try {
         formRef.current?.setErrors({})
 
@@ -29,7 +34,10 @@ const SignIn: React.FC = () => {
 
         await schema.validate(data, { abortEarly: false })
 
-        signIn()
+        signIn({
+          email: data.email,
+          password: data.password,
+        })
       } catch (err) {
         const errors = getValidationErrors(err)
         formRef.current?.setErrors(errors)
