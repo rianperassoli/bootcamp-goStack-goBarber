b89329716158101ff689d1a84c9309a258043309
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-01 23:03:27 -0300

    Renomeada pasta context para hooks

--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -8,7 +8,7 @@ import { Container, Content, Background } from './styles'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import getValidationErrors from '../../utils/getValidationErrors'
-import { useAuth } from '../../context/AuthContext'
+import { useAuth } from '../../hooks/AuthContext'
 
 interface SignInFormData {
   email: string
