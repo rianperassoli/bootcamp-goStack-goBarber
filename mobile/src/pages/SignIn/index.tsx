ce7c9646c8470a77d7e58ff12459b09ae92db7dd
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-03 20:04:15 -0300

    Criada tela de login e melhorados componentes

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Image } from 'react-native'
 
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
 import { Container, Title } from './styles'
 
 import logoImage from '../../assets/logo.png'
@@ -10,6 +13,10 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImage} />
       <Title>Faça seu logon</Title>
+
+      <Input name="email" icon="mail" placeholder="E-mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => console.log('oper')}>Entrar</Button>
     </Container>
   )
 }
