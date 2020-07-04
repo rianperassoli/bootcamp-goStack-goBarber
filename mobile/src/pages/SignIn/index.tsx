9e5dc27f0ac0b1378b7472e399b04af8fa7925a9
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-04 16:15:49 -0300

    adicionado unform e adicionada estrutura de componentes para utilizar o unform

--- mobile/src/pages/SignIn/index.tsx
+++ mobile/src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useCallback, useRef } from 'react'
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -25,6 +27,12 @@ import logoImage from '../../assets/logo.png'
 
 const SignIn: React.FC = () => {
   const navigation = useNavigation()
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit = useCallback((data: object) => {
+    console.log(data)
+  }, [])
+
   return (
     <>
       <KeyboardAvoidingView
@@ -43,11 +51,15 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-            <Button onPress={() => console.log('oper')}>Entrar</Button>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
 
-            <ForgotPassword onPress={() => console.log('boa')}>
+            <ForgotPassword onPress={() => formRef.current?.submitForm()}>
               <ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
             </ForgotPassword>
           </Container>
--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useCallback } from 'react'
 import {
   Image,
   View,
@@ -8,6 +8,8 @@ import {
 } from 'react-native'
 import Icon from 'react-native-vector-icons/Feather'
 import { useNavigation } from '@react-navigation/native'
+import { Form } from '@unform/mobile'
+import { FormHandles } from '@unform/core'
 
 import Input from '../../components/Input'
 import Button from '../../components/Button'
@@ -23,6 +25,11 @@ import logoImage from '../../assets/logo.png'
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation()
+  const formRef = useRef<FormHandles>(null)
+
+  const handleSubmit = useCallback((data: object) => {
+    console.log(data)
+  }, [])
 
   return (
     <>
@@ -42,10 +49,14 @@ const SignUp: React.FC = () => {
               <Title>Crie sua conta</Title>
             </View>
 
-            <Input name="name" icon="user" placeholder="Nome" />
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-            <Button onPress={() => console.log('oper')}>Entrar</Button>
+            <Form ref={formRef} onSubmit={handleSubmit}>
+              <Input name="name" icon="user" placeholder="Nome" />
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+              <Button onPress={() => formRef.current?.submitForm()}>
+                Entrar
+              </Button>
+            </Form>
           </Container>
         </ScrollView>
       </KeyboardAvoidingView>
