d12a0b05d02cd6785287809b4c027d5f61bab919
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-04 15:40:45 -0300

    Criada tela de cadastro e melhorada tela de logon

--- mobile/src/pages/SignUp/index.tsx
+++ mobile/src/pages/SignUp/index.tsx
@@ -1,8 +1,61 @@
 import React from 'react'
-import { Container } from './styles'
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
+import { useNavigation } from '@react-navigation/native'
+
+import Input from '../../components/Input'
+import Button from '../../components/Button'
+
+import {
+  Container,
+  Title,
+  BackToSignInButton,
+  BackToSignInButtonText,
+} from './styles'
+
+import logoImage from '../../assets/logo.png'
 
 const SignUp: React.FC = () => {
-  return <Container />
+  const navigation = useNavigation()
+
+  return (
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          keyboardShouldPersistTaps="handled"
+          contentContainerStyle={{ flex: 1 }}
+        >
+          <Container>
+            <Image source={logoImage} />
+
+            <View>
+              <Title>Crie sua conta</Title>
+            </View>
+
+            <Input name="name" icon="user" placeholder="Nome" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
+            <Button onPress={() => console.log('oper')}>Entrar</Button>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <BackToSignInButton onPress={() => navigation.goBack()}>
+        <Icon name="arrow-left" size={16} color="#fff" />
+        <BackToSignInButtonText>Voltar para logon</BackToSignInButtonText>
+      </BackToSignInButton>
+    </>
+  )
 }
 
 export default SignUp
--- mobile/src/pages/SignUp/styles.ts
+++ mobile/src/pages/SignUp/styles.ts
@@ -1,3 +1,37 @@
 import styled from 'styled-components/native'
 
-export const Container = styled.View``
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+`
+
+export const Title = styled.Text`
+  font-size: 22px;
+  color: #f4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 36px 0 24px;
+`
+
+export const BackToSignInButton = styled.TouchableOpacity`
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: #312e38;
+  border-top-width: 1px;
+  border-color: #232129;
+  padding: 12px 0;
+
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`
+
+export const BackToSignInButtonText = styled.Text`
+  color: #fff;
+  font-size: 12px;
+  font-family: 'RobotoSlab-Regular';
+  margin-left: 12px;
+`
