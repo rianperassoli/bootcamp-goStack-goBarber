dfec8b11ee2950ea5d7078c91bb6a4dea3cc073c
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-14 20:45:41 -0300

    Criado repository fake e criado teste para o service CreateAppointmentService

---
+++ api/src/modules/appointments/repositories/fakes/FakeAppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { uuid } from "uuidv4"
+
+import IAppointmentRepository from "@modules/appointments/repositories/IAppointmentsRepository"
+import ICreateAppointmentDTO from "@modules/appointments/dtos/ICreateAppointmentDTO"
+import Appointment from "@modules/appointments/entities/Appointment"
+
+class FakeAppointmentsRepository implements IAppointmentRepository {
+  private appointments: Appointment[] = []
+
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
+    const appointmentFinded = this.appointments.find(
+      (appointment) => appointment.date === date
+    )
+
+    return appointmentFinded
+  }
+
+  public async create({
+    provider_id,
+    date,
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = new Appointment()
+
+    Object.assign(appointment, { id: uuid(), date, provider_id })
+
+    this.appointments.push(appointment)
+
+    return appointment
+  }
+}
+
+export default FakeAppointmentsRepository
