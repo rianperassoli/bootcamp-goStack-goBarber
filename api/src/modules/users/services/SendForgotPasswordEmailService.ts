9474831ad77d49fc5898a1c61eca874382af9ad1
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-20 20:06:49 -0300

    Criado teste de envio de email sem implementação de funcionalidade

---
+++ api/src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,25 @@
+// import AppError from "@shared/errors/AppError"
+import { injectable, inject } from "tsyringe"
+// import User from "../infra/typeorm/entities/User"
+import IMailProvider from "@shared/container/providers/MailProvider/models/IMailProvider"
+import IUsersRepository from "../repositories/IUsersRepository"
+
+interface IRequestDTO {
+  email: string
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository,
+    @inject("MailProvider")
+    private mailProvider: IMailProvider
+  ) {}
+
+  public async execute({ email }: IRequestDTO): Promise<void> {
+    this.mailProvider.sendMail(email, "Pedido de recuperação de senha recebido")
+  }
+}
+
+export default SendForgotPasswordEmailService
