5b9357bc2aaf133e8e2a2d42d64d5bbfa346c244
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-29 20:55:17 -0300

    Configuração dos envios de email com o SES

---
+++ api/src/config/mail.ts
@@ -0,0 +1,14 @@
+interface IMailConfig {
+  driver: "ethereal" | "ses"
+
+  defaults: {
+    from: {
+      email: string
+      name: string
+    }
+  }
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || "ethereal",
+} as IMailConfig
