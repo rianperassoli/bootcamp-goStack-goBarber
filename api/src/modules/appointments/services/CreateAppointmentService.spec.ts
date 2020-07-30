242d03313726b3c4067dae1ab19e431f8893439a
Rian Perassoli <rian.perassoli@gmail.com>
2020-07-30 20:54:35 -0300

    Correção dos testes

--- api/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ api/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,20 +1,24 @@
 import AppError from "@shared/errors/AppError"
 import FakeNotificationsRepository from "@modules/notifications/repositories/fakes/FakeNotificationsRepository"
+import FakeCacheProvider from "@shared/container/providers/CacheProvider/fakes/FakeCacheProvider"
 import FakeAppointmentsRepository from "../repositories/fakes/FakeAppointmentsRepository"
 import CreateAppointmentService from "./CreateAppointmentService"
 
 let fakeAppointmentRepository: FakeAppointmentsRepository
 let fakeNotificationRepository: FakeNotificationsRepository
+let fakeCacheProvider: FakeCacheProvider
 let createAppointment: CreateAppointmentService
 
 describe("CreateAppointment", () => {
   beforeEach(() => {
     fakeAppointmentRepository = new FakeAppointmentsRepository()
     fakeNotificationRepository = new FakeNotificationsRepository()
+    fakeCacheProvider = new FakeCacheProvider()
 
     createAppointment = new CreateAppointmentService(
       fakeAppointmentRepository,
-      fakeNotificationRepository
+      fakeNotificationRepository,
+      fakeCacheProvider
     )
   })
 
