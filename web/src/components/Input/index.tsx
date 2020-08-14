de6e549afed96082e6273f3ccc0010b61bfbeb20
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-14 17:42:05 -0300

    Criada a tela de profile

--- web/src/components/Input/index.tsx
+++ web/src/components/Input/index.tsx
@@ -13,9 +13,15 @@ import { Container, Error } from './styles'
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string
   icon: React.ComponentType<IconBaseProps>
+  containerStyle?: object
 }
 
-const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  icon: Icon,
+  containerStyle,
+  ...rest
+}) => {
   const inputRef = useRef<HTMLInputElement>(null)
 
   const { fieldName, defaultValue, error, registerField } = useField(name)
@@ -42,7 +48,12 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, [fieldName, registerField])
 
   return (
-    <Container isErrored={!!error} isFilled={isFilled} isFocused={isFocused}>
+    <Container
+      style={containerStyle}
+      isErrored={!!error}
+      isFilled={isFilled}
+      isFocused={isFocused}
+    >
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
