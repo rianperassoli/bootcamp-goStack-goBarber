b1e7f05b642ffb5bd9cf817e73beb579256396a1
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-14 16:15:04 -0300

    Adicionado listagem dos agendamentos

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useEffect, useMemo } from 'react'
-import { isToday, format } from 'date-fns'
+import { isToday, format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 import { FiPower, FiClock } from 'react-icons/fi'
 import DayPicker, { DayModifiers } from 'react-day-picker'
@@ -29,6 +29,7 @@ interface MonthAvailabilityItem {
 interface Appointment {
   id: string
   date: string
+  hour_formatted: string
   user: {
     name: string
     avatar_url: string
@@ -60,7 +61,7 @@ const Dashboard: React.FC = () => {
 
   useEffect(() => {
     api
-      .get('/appointments/me', {
+      .get<Appointment[]>('/appointments/me', {
         params: {
           year: selectedDate.getFullYear(),
           month: selectedDate.getMonth() + 1,
@@ -68,8 +69,14 @@ const Dashboard: React.FC = () => {
         },
       })
       .then(response => {
-        setAppointments(response.data)
-        console.log(response.data)
+        const appointmentsFormatted = response.data.map(appointment => {
+          return {
+            ...appointment,
+            hour_formatted: format(parseISO(appointment.date), 'HH:mm'),
+          }
+        })
+
+        setAppointments(appointmentsFormatted)
       })
   }, [selectedDate])
 
@@ -104,6 +111,18 @@ const Dashboard: React.FC = () => {
   const handleMonthChange = useCallback((month: Date) => {
     setCurrentMonth(month)
   }, [])
+
+  const morningAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() < 12
+    })
+  }, [appointments])
+
+  const afternoonAppointments = useMemo(() => {
+    return appointments.filter(appointment => {
+      return parseISO(appointment.date).getHours() >= 12
+    })
+  }, [appointments])
 
   return (
     <Container>
@@ -151,48 +170,45 @@ const Dashboard: React.FC = () => {
           <Section>
             <strong>Manhã</strong>
 
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
+            {morningAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.hour_formatted}
+                </span>
 
-              <div>
-                <img src={user.avatar_url} alt={user.name} />
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
 
-                <strong>Rian Perassoli</strong>
-              </div>
-            </Appointment>
-
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
-
-              <div>
-                <img src={user.avatar_url} alt={user.name} />
-
-                <strong>Rian Perassoli</strong>
-              </div>
-            </Appointment>
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
 
           <Section>
             <strong>Tarde</strong>
 
-            <Appointment>
-              <span>
-                <FiClock />
-                08:00
-              </span>
+            {afternoonAppointments.map(appointment => (
+              <Appointment key={appointment.id}>
+                <span>
+                  <FiClock />
+                  {appointment.hour_formatted}
+                </span>
 
-              <div>
-                <img src={user.avatar_url} alt={user.name} />
+                <div>
+                  <img
+                    src={appointment.user.avatar_url}
+                    alt={appointment.user.name}
+                  />
 
-                <strong>Rian Perassoli</strong>
-              </div>
-            </Appointment>
+                  <strong>{appointment.user.name}</strong>
+                </div>
+              </Appointment>
+            ))}
           </Section>
         </Schedule>
 
