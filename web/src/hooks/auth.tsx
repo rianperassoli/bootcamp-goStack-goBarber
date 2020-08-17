ec621cbd2efa7cbf9fd4fd302b6c3f80db598816
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-17 19:15:34 -0300

    Criada alteração e atualizaçõa da imagem do perfil

--- web/src/hooks/auth.tsx
+++ web/src/hooks/auth.tsx
@@ -17,6 +17,7 @@ interface AuthContextData {
   user: User
   signIn(credentials: SignInCredentials): Promise<void>
   signOut(): void
+  updateUser(user: User): void
 }
 
 interface AuthState {
@@ -63,8 +64,22 @@ const AuthProvider: React.FC = ({ children }) => {
     setData({} as AuthState)
   }, [])
 
+  const updateUser = useCallback(
+    (user: User) => {
+      setData({
+        token: data.token,
+        user,
+      })
+
+      localStorage.setItem('@GoBarber:user', JSON.stringify(user))
+    },
+    [data.token],
+  )
+
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider
+      value={{ user: data.user, signIn, signOut, updateUser }}
+    >
       {children}
     </AuthContext.Provider>
   )
