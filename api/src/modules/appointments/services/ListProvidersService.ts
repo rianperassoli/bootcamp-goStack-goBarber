1a3a719fb30f03c8aee56a57f225b70564854f5f
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-20 15:43:54 -0300

    Ajustes para retornar campos corretos

--- api/src/modules/appointments/services/ListProvidersService.ts
+++ api/src/modules/appointments/services/ListProvidersService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from "tsyringe"
 import User from "@modules/users/infra/typeorm/entities/User"
 import IUsersRepository from "@modules/users/repositories/IUsersRepository"
 import ICacheProvider from "@shared/container/providers/CacheProvider/models/ICacheProvider"
+import { classToClass } from "class-transformer"
 
 interface IRequestDTO {
   user_id: string
@@ -26,7 +27,10 @@ class ListProvidersService {
         except_user_id: user_id,
       })
 
-      await this.cacheProvider.save(`providers-list:${user_id}`, users)
+      await this.cacheProvider.save(
+        `providers-list:${user_id}`,
+        classToClass(users)
+      )
     }
 
     return users
