ef87997fc91b6112f2221810fb2fb1c79c903abe
Rian Perassoli <rian.perassoli@gmail.com>
2020-08-26 19:29:04 -0300

    Criada estilização da tela de agendamento criado e realizada navegaçao para o dashboard

--- mobile/src/pages/AppointmentCreated/index.tsx
+++ mobile/src/pages/AppointmentCreated/index.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import { Text, Button } from 'react-native'
+import React, { useCallback } from 'react'
+import Icon from 'react-native-vector-icons/Feather'
 
+import { useNavigation } from '@react-navigation/native'
 import { useAuth } from '../../hooks/auth'
 
-import { Container } from './styles'
+import { Container, Title, Description, OkButton, OkButtonText } from './styles'
 
 const AppointmentCreated: React.FC = () => {
-  const { signOut } = useAuth()
+  const { reset } = useNavigation()
+
+  const handleOkPressed = useCallback(() => {
+    reset({
+      routes: [{ name: 'Dashboard' }],
+      index: 0,
+    })
+  }, [reset])
+
   return (
     <Container>
-      <Text>AppointmentCreated</Text>
-      <Button title="Sair" onPress={signOut} />
+      <Icon name="check" size={80} color="#04d361" />
+
+      <Title>Agendamento Concluido</Title>
+      <Description>Agendamento Concluido</Description>
+
+      <OkButton>
+        <OkButtonText onPress={handleOkPressed}>Ok</OkButtonText>
+      </OkButton>
     </Container>
   )
 }
